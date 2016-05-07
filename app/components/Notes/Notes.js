2e70777f0845e0d387368589e3abf51fe16ddc7a
SanthoshKK <kksant@gmail.com>
2016-05-07 20:21:47 +0530

    upto chapter 5

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,11 +1,14 @@
 var React = require('react');
+var NotesList = require('./NotesList');
 
 var Notes = React.createClass({
+
 	render: function(){
+		console.log('Notes: ', this.props.notes)
 		return(
 		<div>
-		<p>NOTES</p>
-		<p>Notes:{this.props.notes}</p>
+		<h3>Notes for User: {this.props.username} </h3>
+			<NotesList notes={this.props.notes}/>
 		</div>
 		)
 	}
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,9 +3,13 @@ var Router = require('react-router');
 var Repo = require('./GitHub/Repo');
 var UserProfile = require('./GitHub/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var FireBase = require('firebase');
 
 
 var Profile = React.createClass({
+
+	mixins: [ReactFireMixin],
 
 	getInitialState: function(){
 		return{
@@ -15,19 +19,28 @@ var Profile = React.createClass({
 		}
 	},
 
+	componentDidMount: function(){
+		this.ref = new FireBase('https://github-note-taker.firebaseio.com/');
+		var childRef = this.ref.child(this.props.params.username);
+		this.bindAsArray(childRef, 'notes');
+	},
+
+	componentWillUnMount: function(){
+		this.bind('notes');
+	},
+
 
 	render: function(){
-		console.log(this.props) 
 		return(
 			<div className="row">
 				<div className="col-md-4">
 				<UserProfile username={this.props.params.username} bio={this.state.bio}/>
 				</div>
 				<div className="col-md-4">
-				<Repo repos={this.state.repos}/>
+				<Repo username={this.props.params.username} repos={this.state.repos}/>
 				</div>
 				<div className="col-md-4">
-				<Notes notes={this.state.notes}/>
+				<Notes username={this.props.params.username} notes={this.state.notes}/>
 				</div>
 			</div>
 			)
